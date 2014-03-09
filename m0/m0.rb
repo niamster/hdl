a1f8769d5e18ea222695b37642ba2fe8b50eaf55
Dmytro Milinevskyy <milinevskyy@gmail.com>
2014-03-09 23:05:44 +0100

    m0: extended pin configuration

--- m0/m0.rb
+++ m0/m0.rb
@@ -2,10 +2,19 @@
   target: {family: 'Cyclone IV E', device: 'EP4CE22F17C6'},
   top: 'm0_de0nano',
   pins: {
-    sys_clk: 'R8',
-    key: 'J15',
-    rstn: 'E1',
-    leds: ['A15', 'A13', 'B13', 'A11', 'D1', 'F3', 'B1', 'L3']
+    sys_clk: {pad: 'R8', voltage: "3.3-V LVTTL",},
+    key: {pad: 'J15', voltage: "3.3-V LVTTL",},
+    rstn: {pad: 'E1', voltage: "3.3-V LVTTL",},
+    leds: [
+           {pad: 'A15', voltage: "3.3-V LVTTL", current: '4mA'},
+           {pad: 'A13', voltage: "3.3-V LVTTL", current: '4mA'},
+           {pad: 'B13', voltage: "3.3-V LVTTL", current: '4mA'},
+           {pad: 'A11', voltage: "3.3-V LVTTL", current: '4mA'},
+           {pad: 'D1', voltage: "3.3-V LVTTL", current: '4mA'},
+           {pad: 'F3', voltage: "3.3-V LVTTL", current: '4mA'},
+           {pad: 'B1', voltage: "3.3-V LVTTL", current: '4mA'},
+           {pad: 'L3', voltage: "3.3-V LVTTL", current: '4mA'},
+          ]
   },
   sdc: '../boards/de0-nano.sdc',
   # files: ['m0.v'],
